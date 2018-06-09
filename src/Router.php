0503cb4c4549ffdd46e42455dfbfd953e3134f06
Shudd3r <q3.shudder@gmail.com>
2018-06-09 13:03:28 +0200

    Added phpdoc in Router class

--- src/Router.php
+++ src/Router.php
@@ -20,28 +20,54 @@ use Psr\Http\Message\UriInterface;
 class Router implements RequestHandlerInterface
 {
     private $route;
-    private $uriPrototype;
-    private $notFound;
+    private $baseUri;
+    private $baseResponse;
 
-    public function __construct(Route $route, UriInterface $uriPrototype, ResponseInterface $notFound)
+    /**
+     * Response prototype is used for responses handled within routing tree
+     * without delegating request to endpoint handler. This allows routing
+     * to avoid dependency on concrete ResponseInterface implementation,
+     * utilise Null Object Pattern and communicating internally.
+     *
+     * For example:
+     * - same instance may indicate unprocessed response (NullResponse)
+     * - conditional redirect might be returned based on middleware context
+     * - resource methods might be inspected within routing (OPTIONS method)
+     *
+     * @param Route             $route
+     * @param UriInterface      $baseUri      prototype on which endpoint uri will be built
+     * @param ResponseInterface $baseResponse prototype used as internal router response
+     */
+    public function __construct(Route $route, UriInterface $baseUri, ResponseInterface $baseResponse)
     {
         $this->route        = $route;
-        $this->uriPrototype = $uriPrototype;
-        $this->notFound     = $notFound;
+        $this->baseUri      = $baseUri;
+        $this->baseResponse = $baseResponse;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return $this->route->forward($request, $this->notFound);
+        return $this->route->forward($request, $this->baseResponse);
     }
 
+    /**
+     * @param string $path   by default dot separated list of switch identifiers
+     * @param array  $params named or ordered variables for dynamic uri patterns
+     *
+     * @return UriInterface
+     */
     public function uri(string $path, array $params = []): UriInterface
     {
-        return $this->route->gateway($path)->uri($this->uriPrototype, $params);
+        return $this->route->gateway($path)->uri($this->baseUri, $params);
     }
 
+    /**
+     * @param string $path
+     *
+     * @return Router with changed root context
+     */
     public function route(string $path): Router
     {
-        return new static($this->route->gateway($path), $this->uriPrototype, $this->notFound);
+        return new static($this->route->gateway($path), $this->baseUri, $this->baseResponse);
     }
 }
