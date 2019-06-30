a3e1ba9d76a6a87071638b51ad65ed244dcc4c8e
Shudd3r <q3.shudder@gmail.com>
2019-06-30 13:20:51 +0200

    Added phpDoc comments for Builder class

--- src/Builder.php
+++ src/Builder.php
@@ -22,7 +22,6 @@ use Psr\Http\Message\UriInterface;
 
 class Builder
 {
-    /** @var MappedRoutes */
     private $mappedRoutes;
 
     /** @var Node */
@@ -31,6 +30,17 @@ class Builder
     /** @var Router */
     private $router;
 
+    /**
+     * If object is instantiated without $mappedRoutes parameter
+     * it will be created internally with its own router callback,
+     * and methods using mapping procedures will be later inaccessible
+     * (Exception\BuilderLogicException).
+     *
+     * Also if provided MappedRoutes instance does not define router
+     * callback it will be set by this class.
+     *
+     * @param null|MappedRoutes $mappedRoutes
+     */
     public function __construct(MappedRoutes $mappedRoutes = null)
     {
         $routerCallback = function () { return $this->router; };
@@ -41,11 +51,39 @@ class Builder
         }
     }
 
+    /**
+     * Produces Builder with predefined configuration for mapping
+     * endpoints and gates using string identifier.
+     *
+     * @see MappedRoutes
+     *
+     * @param ContainerInterface $container
+     *
+     * @return Builder
+     */
     public static function withContainer(ContainerInterface $container): self
     {
         return new self(MappedRoutes::withContainerMapping($container));
     }
 
+    /**
+     * Creates Router instance based on builder nodes defined on root
+     * node.
+     *
+     * Throws BuilderLogicException if routes are not (fully) defined
+     * and builder nodes cannot be resolved into Route instances.
+     *
+     *
+     * @param UriInterface      $baseUri      prototype for uri instances produced with
+     *                                        Router::uri() method
+     * @param ResponseInterface $nullResponse recognized by Router as a product of unprocessed
+     *                                        request (from Router::handle() method) or a base for
+     *                                        creating response within one of its Routes
+     *
+     * @throws Exception\BuilderLogicException
+     *
+     * @return Router
+     */
     public function router(UriInterface $baseUri, ResponseInterface $nullResponse): Router
     {
         if (!$this->builder) {
@@ -54,6 +92,12 @@ class Builder
         return $this->router = new Router($this->builder->build(), $baseUri, $nullResponse);
     }
 
+    /**
+     * Returns builder node that produces complex Route as entry point
+     * for Router built with router() method.
+     *
+     * @return Node\RouteNode
+     */
     public function rootNode(): Node\RouteNode
     {
         if ($this->builder) {
@@ -62,11 +106,38 @@ class Builder
         return $this->builder = new Node\RouteNode(new Context($this->mappedRoutes));
     }
 
+    /**
+     * Creates builder node that can produce complex Route instance
+     * just as one used by Router builder (built on root node), but
+     * itself is not connected to any of Router's nodes (unless you
+     * connect this route passing it as a parameter to one of router
+     * node methods).
+     *
+     * Throws BuilderLogicException if routes are not (fully) defined
+     * and builder nodes cannot be resolved into Route instances.
+     *
+     * NOTE: Redirect endpoint uses Router callback, so if router is
+     * not built with this Builder class and you want to use redirect
+     * for detached route only make sure you provide valid router
+     * callback within MappedRoutes constructor argument.
+     *
+     * @throws Exception\BuilderLogicException
+     *
+     * @return Node\RouteNode
+     */
     public function detachedNode(): Node\RouteNode
     {
         return new Node\RouteNode(new Context($this->mappedRoutes));
     }
 
+    /**
+     * Creates builder that can produce single, linear Route.
+     * This route can use various gates and will normally lead
+     * to some endpoint, but can also connect other, more
+     * complex routes.
+     *
+     * @return Builder\EndpointRouteBuilder
+     */
     public function route(): Builder\EndpointRouteBuilder
     {
         return new Builder\EndpointRouteBuilder(new Context($this->mappedRoutes));
