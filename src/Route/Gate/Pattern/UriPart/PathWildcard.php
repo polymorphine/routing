acc96a3bb2ab3b789777d1ab9909e36892780ba1
Shudd3r <q3.shudder@gmail.com>
2019-08-17 23:35:04 +0200

    Added PathWildcard pattern

---
+++ src/Route/Gate/Pattern/UriPart/PathWildcard.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Routing package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Routing\Route\Gate\Pattern\UriPart;
+
+use Polymorphine\Routing\Route;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
+
+
+/**
+ * Pattern capturing unprocessed path.
+ */
+class PathWildcard implements Route\Gate\Pattern
+{
+    use Route\Gate\Pattern\PathContextMethods;
+
+    public function __construct()
+    {
+    }
+
+    public function matchedRequest(ServerRequestInterface $request): ?ServerRequestInterface
+    {
+        $path = implode('/', $this->relativePath($request));
+        return $request->withAttribute(Route::WILDCARD_ATTRIBUTE, $path)->withAttribute(Route::PATH_ATTRIBUTE, []);
+    }
+
+    public function uri(UriInterface $prototype, array $params): UriInterface
+    {
+        return $prototype;
+    }
+}
---
+++ tests/Route/Gate/Pattern/UriPart/PathWildcardTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of Polymorphine/Routing package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Polymorphine\Routing\Tests\Route\Gate\Pattern\UriPart;
+
+use PHPUnit\Framework\TestCase;
+use Polymorphine\Routing\Route;
+use Polymorphine\Routing\Tests\Doubles;
+
+
+class PathWildcardTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(Route\Gate\Pattern::class, new Route\Gate\Pattern\UriPart\PathWildcard());
+    }
+
+    public function testMatchedRequestMethod_ReturnsRequestWithCapturedAndClearedRelativePath()
+    {
+        $pattern = new Route\Gate\Pattern\UriPart\PathWildcard();
+
+        $request = $pattern->matchedRequest($this->request('/foo/bar/baz'));
+        $this->assertSame('foo/bar/baz', $request->getAttribute(Route::WILDCARD_ATTRIBUTE));
+        $this->assertSame([], $request->getAttribute(Route::PATH_ATTRIBUTE));
+
+        $request = $pattern->matchedRequest($this->request('/foo/bar/baz', 'bar/baz'));
+        $this->assertSame('bar/baz', $request->getAttribute(Route::WILDCARD_ATTRIBUTE));
+        $this->assertSame([], $request->getAttribute(Route::PATH_ATTRIBUTE));
+
+        $request = $pattern->matchedRequest($this->request('/'));
+        $this->assertSame('', $request->getAttribute(Route::WILDCARD_ATTRIBUTE));
+        $this->assertSame([], $request->getAttribute(Route::PATH_ATTRIBUTE));
+    }
+
+    public function testUriMethod_ReturnsPrototype()
+    {
+        $pattern   = new Route\Gate\Pattern\UriPart\PathWildcard();
+        $prototype = Doubles\FakeUri::fromString('http://example.com/foo/bar?query=baz');
+
+        $this->assertSame($prototype, $pattern->uri($prototype, ['anything' => 'xxx']));
+    }
+
+    private function request(string $uri, string $relativePath = null)
+    {
+        $request = new Doubles\FakeServerRequest('GET', Doubles\FakeUri::fromString($uri));
+        return $relativePath ? $request->withAttribute(Route::PATH_ATTRIBUTE, explode('/', $relativePath)) : $request;
+    }
+}
